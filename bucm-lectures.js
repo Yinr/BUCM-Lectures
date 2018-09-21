7d57a4919a389ba70a22f06ad02b1074879e10ca
Yinr <yinr@yinr.cc>
2018-09-21 17:21:55 +0800

    init

---
+++ bucm-lectures.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: '#bucm-lectures',
+    data: {
+        items: [{
+            id: 28190609,
+            title: "三十年中美临床感悟-田海河博士",
+            date: "2018.9.21",
+            time: "14:00",
+            classroom: "东-9教",
+            url: "https://a.xiumi.us/stage/v5/37Dxv/107159455"
+        }]
+    }
+})
