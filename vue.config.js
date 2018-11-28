b6efc0e9761154f17ba9b568241f43b72c1cc623
Yinr <yinr@yinr.cc>
2018-11-29 00:12:09 +0800

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   pwa: {
     name: 'BUCM Lectures'
-  }
+  },
+
+  baseUrl: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: false,
+  parallel: undefined,
+  css: undefined
 }
