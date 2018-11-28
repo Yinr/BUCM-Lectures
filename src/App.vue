38bf89b28053d488b2dd0ef1ad8c09c0c9bfbae2
Yinr <yinr@yinr.cc>
2018-11-28 23:03:23 +0800

    update lecture on 2018.11.30

--- src/App.vue
+++ src/App.vue
@@ -44,6 +44,12 @@ export default {
         time: "2018-11-22 15:30:00",
         classroom: "东-9教",
         url: "https://r.xiumi.us/stage/v5/37Dxv/115770659"
+      }, {
+        id: 29674324,
+        title: "中医如何走进人工智能时代？",
+        time: "2018-11-30 9:00:00",
+        classroom: "西-三报",
+        url: "https://r.xiumi.us/stage/v5/37Dxv/113436024"
       }]
     }
   },
