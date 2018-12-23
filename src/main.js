06bb91cb9d20e32a3bc51968aa6b3aaa722b33c1
Yinr <yinr@yinr.cc>
2018-12-23 14:57:16 +0800

    switch to iview

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import './plugins/axios'
 import App from './App.vue'
 import './registerServiceWorker'
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
 
-Vue.use(Buefy)
 Vue.config.productionTip = false
+Vue.use(iView);
 
 new Vue({
   render: h => h(App)
