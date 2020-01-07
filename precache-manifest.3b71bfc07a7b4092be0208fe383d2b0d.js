afefe629f9227eb16185477bb4a5cd3af1a74d7e
Yinr <yinr@yinr.cc>
2020-01-07 04:24:03 +0000

    Travis Publish

---
+++ precache-manifest.3b71bfc07a7b4092be0208fe383d2b0d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0fcac61706b0f8d2d0c",
+    "url": "/css/app.2efbc927.css"
+  },
+  {
+    "revision": "b0fcac61706b0f8d2d0c",
+    "url": "/js/app.b0affd9a.js"
+  },
+  {
+    "revision": "3e7a604bc169a8232a68",
+    "url": "/css/chunk-vendors.77555ead.css"
+  },
+  {
+    "revision": "3e7a604bc169a8232a68",
+    "url": "/js/chunk-vendors.3e869023.js"
+  },
+  {
+    "revision": "f3f07174e70f5a924ee8bf14043f4abb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27e297abf1d903faaedf7e9476aa20dd",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "038239ab34c7c75348f8c904545f0343",
+    "url": "/data/lectures.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
