6eafe771fb5ce9a224f5f4bb3e0e4908a2a42f29
Yinr <yinr@yinr.cc>
2020-01-07 14:27:51 +0800

    update footer year

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
 <footer class="footer">
   <small class="content has-text-centered">
     <p>
-      <span>Copyright &copy; 2018 <a href="https://yinr.cc">Yinr</a>.</span>
+      <span>Copyright &copy; 2018-2020 <a href="https://yinr.cc">Yinr</a>.</span>
     </p>
   </small>
 </footer>
